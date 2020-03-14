d4c45e3ec70da6cd119a5f126c0afa8f2c314651
Ivo Limmen <ivo.limmen@qsd.nl>
2020-03-14 10:20:50 +0100

    Urls are now better matched.

--- storage/postgres/src/main/java/org/limmen/mystart/postgres/DbLinkStorage.java
+++ storage/postgres/src/main/java/org/limmen/mystart/postgres/DbLinkStorage.java
@@ -5,7 +5,6 @@ import java.util.Collection;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-
 import lombok.extern.slf4j.Slf4j;
 import org.limmen.mystart.Link;
 import org.limmen.mystart.LinkStorage;
@@ -39,119 +38,100 @@ public class DbLinkStorage extends DbAbstractStorage implements LinkStorage {
     String sql = "insert into links (user_id, description, source, title, url, labels, creation_date) "
         + "values (?, ?, ?, ?, ?, ?, ?)";
     executeSql(sql, stmt -> {
-             stmt.setLong(1, userId);
-             stmt.setString(2, link.getDescription());
-             stmt.setString(3, link.getSource());
-             stmt.setString(4, link.getTitle());
-             stmt.setString(5, link.getUrl());
-             stmt.setStringArray(6, link.getLabels().toArray(new String[link.getLabels().size()]));
-             stmt.setLocalDate(7, link.getCreationDate());
-           });
+      stmt.setLong(1, userId);
+      stmt.setString(2, link.getDescription());
+      stmt.setString(3, link.getSource());
+      stmt.setString(4, link.getTitle());
+      stmt.setString(5, link.getUrl());
+      stmt.setStringArray(6, link.getLabels().toArray(new String[link.getLabels().size()]));
+      stmt.setLocalDate(7, link.getCreationDate());
+    });
   }
 
   @Override
   public Link get(Long userId, Long id) {
     return executeSqlSingle("select * from links where user_id = ? and id = ?", args -> {
-                          args.setLong(1, userId);
-                          args.setLong(2, id);
-                        }, LINK_MAPPER);
+      args.setLong(1, userId);
+      args.setLong(2, id);
+    }, LINK_MAPPER);
   }
 
   @Override
   public Collection<Link> getAll(Long userId) {
     return executeSql("select * from links where user_id = ? order by title asc", args -> {
-                    args.setLong(1, userId);
-                  }, LINK_MAPPER);
+      args.setLong(1, userId);
+    }, LINK_MAPPER);
   }
 
   @Override
   public Collection<Link> getAllByLabel(Long userId, String label) {
     return executeSql("select * from links where user_id = ? and labels && array[?] order by title asc", args -> {
-                    args.setLong(1, userId);
-                    args.setStringArray(2, new String[]{label});
-                  }, LINK_MAPPER);
+      args.setLong(1, userId);
+      args.setStringArray(2, new String[]{label});
+    }, LINK_MAPPER);
   }
 
   @Override
   public Collection<String> getAllLabels(Long userId) {
     return executeSqlStringCollection("select distinct unnest(labels) from links where user_id = ? order by 1", args -> {
-                                    args.setLong(1, userId);
-                                  });
+      args.setLong(1, userId);
+    });
   }
 
   @Override
   public Link getByUrl(Long userId, String url) {
 
-    if (url.startsWith("https://")) {
-      url = url.substring(8);
-    } else if (url.startsWith("http://")) {
-      url = url.substring(7);
-    }
     if (url.endsWith("/")) {
       url = url.substring(0, url.length() - 1);
-    }    
-    String strippedUrl = url;
+    }
+    String strippedUrl = url.toLowerCase();
     log.debug("Searching for url: {}", strippedUrl);
 
-    String sql = "select t.* from (select l.id, "
-        + "case "
-        + "  when strpos(l.url, 'https://') = 1 "
-        + "  then translate(substr(l.url, 9), '/', '') "
-        + "  when strpos(l.url, 'http://') = 1 "
-        + "  then translate(substr(l.url, 8), '/', '') "
-        + "  else l.url "
-        + "  end, "
-        + "  l.title, "
-        + "  l.creation_date, "
-        + "  l.source, "
-        + "  l.labels, "
-        + "  l.description, "
-        + "  l.private_network, "
-        + "  l.last_check, "
-        + "  l.check_result, "
-        + "  l.last_visit, "
-        + "  l.user_id "
-        + "from links l) t "
-        + "where t.user_id = ? and t.url = ?";
-    return executeSqlSingle(sql, args -> {
-                          args.setLong(1, userId);
-                          args.setString(2, strippedUrl);
-                        }, LINK_MAPPER);
+    return executeSqlSingle("select * from links where user_id = ? and lower(url) = ?", args -> {
+      args.setLong(1, userId);
+      args.setString(2, strippedUrl);
+    }, LINK_MAPPER);
   }
 
   @Override
   public Collection<Link> getLastCreated(Long userId, int limit) {
     return executeSql("select * from links where user_id = ? order by creation_date desc", args -> {
-                    args.setLong(1, userId);
-                    args.setMaxRows(limit);
-                  }, LINK_MAPPER);
+      args.setLong(1, userId);
+      args.setMaxRows(limit);
+    }, LINK_MAPPER);
   }
 
   @Override
   public Collection<Link> getLastVisited(Long userId, int limit) {
     return executeSql("select l.* from links l join visits v on l.id = v.link_id where user_id = ? order by v.visit desc", args -> {
-                    args.setLong(1, userId);
-                    args.setMaxRows(limit);
-                  }, LINK_MAPPER);
+      args.setLong(1, userId);
+      args.setMaxRows(limit);
+    }, LINK_MAPPER);
   }
 
   @Override
   public void importCollection(Long userId, Collection<Link> links, boolean skipDuplicates) {
+    AtomicInteger updated = new AtomicInteger(0);
+    AtomicInteger created = new AtomicInteger(0);
     links.forEach(l -> {
+      log.debug("Searching for URL: {}", l.getUrl());
       Link link = getByUrl(userId, l.getUrl());
       if (link == null) {
         log.info("Creating link {}", l.getUrl());
         create(userId, l);
+        created.incrementAndGet();
       } else {
         if (!skipDuplicates) {
-          log.info("Updating link {}", l.getUrl());
+          log.info("Updating labels of link {}", l.getUrl());
           link.addLabels(l.getLabels());
           update(userId, link);
+          updated.incrementAndGet();
         } else {
           log.info("Skipping link {}", l.getUrl());
         }
       }
     });
+    log.info("Updated {} links, created {} links", updated.get(), created.get());
   }
 
   @Override
@@ -163,16 +143,16 @@ public class DbLinkStorage extends DbAbstractStorage implements LinkStorage {
         + "order by v.visit desc limit 20";
 
     return executeSql(sql, args -> {
-                    args.setLong(1, userId);
-                  }, LINK_MAPPER);
+      args.setLong(1, userId);
+    }, LINK_MAPPER);
   }
 
   @Override
   public void remove(Long userId, Long id) {
     executeSql("delete from links where id = ? and user_id = ?", stmt -> {
-             stmt.setLong(1, id);
-             stmt.setLong(2, userId);
-           });
+      stmt.setLong(1, id);
+      stmt.setLong(2, userId);
+    });
   }
 
   @Override
@@ -185,22 +165,22 @@ public class DbLinkStorage extends DbAbstractStorage implements LinkStorage {
     }
 
     sql.append(criteria.stream()
-      .map(c -> c.toSQL())
-      .collect(Collectors.joining(" or ")));
+        .map(c -> c.toSQL())
+        .collect(Collectors.joining(" or ")));
     sql.append(") order by l.title asc");
 
     AtomicInteger index = new AtomicInteger(0);
     return executeSql(sql.toString(), args -> {
-                    args.setLong(index.incrementAndGet(), userId);
-                    criteria.forEach(c -> {
-                      if (c.getValueType().equals(String.class)) {
-                        args.setString(index.incrementAndGet(), "%" + c.getValue() + "%");
-                      }
-                      if (c.getValueType().equals(String[].class)) {
-                        args.setString(index.incrementAndGet(), (String) c.getValue());
-                      }
-                    });
-                  }, LINK_MAPPER);
+      args.setLong(index.incrementAndGet(), userId);
+      criteria.forEach(c -> {
+        if (c.getValueType().equals(String.class)) {
+          args.setString(index.incrementAndGet(), "%" + c.getValue() + "%");
+        }
+        if (c.getValueType().equals(String[].class)) {
+          args.setString(index.incrementAndGet(), (String) c.getValue());
+        }
+      });
+    }, LINK_MAPPER);
   }
 
   @Override
@@ -218,18 +198,18 @@ public class DbLinkStorage extends DbAbstractStorage implements LinkStorage {
         + "where id = ?"
         + "and user_id = ?";
     executeSql(sql, stmt -> {
-             stmt.setString(1, link.getDescription());
-             stmt.setString(2, link.getTitle());
-             stmt.setString(3, link.getSource());
-             stmt.setString(4, link.getUrl());
-             stmt.setStringArray(5, link.getLabels().toArray(new String[link.getLabels().size()]));
-             stmt.setBool(6, link.isPrivateNetwork());
-             stmt.setString(7, link.getCheckResult());
-             stmt.setLocalDate(8, link.getLastCheck());
-             stmt.setLocalDate(9, link.getLastVisit());
+      stmt.setString(1, link.getDescription());
+      stmt.setString(2, link.getTitle());
+      stmt.setString(3, link.getSource());
+      stmt.setString(4, link.getUrl());
+      stmt.setStringArray(5, link.getLabels().toArray(new String[link.getLabels().size()]));
+      stmt.setBool(6, link.isPrivateNetwork());
+      stmt.setString(7, link.getCheckResult());
+      stmt.setLocalDate(8, link.getLastCheck());
+      stmt.setLocalDate(9, link.getLastVisit());
 
-             stmt.setLong(10, link.getId());
-             stmt.setLong(11, userId);
-           });
+      stmt.setLong(10, link.getId());
+      stmt.setLong(11, userId);
+    });
   }
 }
