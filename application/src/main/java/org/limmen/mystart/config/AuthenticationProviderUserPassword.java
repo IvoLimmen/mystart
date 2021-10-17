ef4c36b75083fa7263a1d28b34d60ec45740bf17
Ivo Limmen <ivo@limmen.org>
2021-10-17 19:06:03 +0200

    Upgraded to micronaut 3.1.0 Added docker compose file for database.

--- application/src/main/java/org/limmen/mystart/config/AuthenticationProviderUserPassword.java
+++ application/src/main/java/org/limmen/mystart/config/AuthenticationProviderUserPassword.java
@@ -1,6 +1,5 @@
 package org.limmen.mystart.config;
 
-import java.util.Collections;
 import java.util.Map;
 import java.util.Optional;
 
@@ -9,42 +8,40 @@ import org.limmen.mystart.UserStorage;
 import org.reactivestreams.Publisher;
 
 import io.micronaut.http.HttpRequest;
-import io.micronaut.security.authentication.AuthenticationException;
-import io.micronaut.security.authentication.AuthenticationFailed;
 import io.micronaut.security.authentication.AuthenticationProvider;
 import io.micronaut.security.authentication.AuthenticationRequest;
 import io.micronaut.security.authentication.AuthenticationResponse;
-import io.micronaut.security.authentication.UserDetails;
-import io.reactivex.Maybe;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
 
 @Singleton
 public class AuthenticationProviderUserPassword implements AuthenticationProvider {
 
-    private SettingsProvider settingsConfig;
-    private UserStorage userStorage;
+    private final SettingsProvider settingsConfig;
+    private final UserStorage userStorage;
+    private final String serverSalt;
 
     @Inject
     public AuthenticationProviderUserPassword(UserStorage userStorage, SettingsProvider settingsConfig) {
         this.userStorage = userStorage;
         this.settingsConfig = settingsConfig;
+        this.serverSalt = this.settingsConfig.getProperties().getProperty("server.salt");
     }
 
     @Override
     public Publisher<AuthenticationResponse> authenticate(HttpRequest<?> httpRequest, AuthenticationRequest<?, ?> authenticationRequest) {
-        return Maybe.<AuthenticationResponse>create(emitter -> {
+        return Flux.create(emitter -> {
             String email = authenticationRequest.getIdentity().toString();
             String password = authenticationRequest.getSecret().toString();
             Optional<User> user = this.userStorage.getByEmail(email);
-            
-            if (user.isPresent() && user.get().check(this.settingsConfig.getProperties().getProperty("server.salt"), password)) {
-                UserDetails details = new UserDetails(email, Collections.emptyList());
-                details.setAttributes(Map.of("id", user.get().getId()));
-                emitter.onSuccess(details);
+            if (user.isPresent() && user.get().check(this.serverSalt, password)) {
+                emitter.next(AuthenticationResponse.success((String) authenticationRequest.getIdentity(), Map.of("id", user.get().getId())));
+                emitter.complete();
             } else {
-                emitter.onError(new AuthenticationException(new AuthenticationFailed()));
+                emitter.error(AuthenticationResponse.exception());
             }
-        }).toFlowable();
+        }, FluxSink.OverflowStrategy.ERROR);        
     }
 }
